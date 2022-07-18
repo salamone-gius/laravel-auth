725ce074191b99bd4e31c0187acd5480b9cf6a5b
salamone-gius <salamone.gius@gmail.com>
2022-07-18 18:58:18 +0200

    Vue.js scaffolding created

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
+// creo le direttive per compilare scss e js
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/front.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/front.scss', 'public/css');
