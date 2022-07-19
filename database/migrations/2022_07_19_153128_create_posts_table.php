3a3bd56cfcbce5c4502c7dc0c97c639aab006325
salamone-gius <salamone.gius@gmail.com>
2022-07-19 17:52:56 +0200

    'posts' table created

--- database/migrations/2022_07_19_153128_create_posts_table.php
+++ database/migrations/2022_07_19_153128_create_posts_table.php
@@ -15,6 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200);
+            // lo slug è la parte di uri che identifica il post (ovviamente univoca). Conviene che non ci siano numeri o id, ma che sia uguale o simile al titolo del post per rendere l'url più 'parlante' e indicizzabile dai motori di ricerca (SEO friendly)
+            $table->string('slug')->unique();
+            $table->text('content');
+            // flag che pubblica o meno un post. Di default non sarà pubblicato
+            $table->boolean('published')->default(false);
             $table->timestamps();
         });
     }
