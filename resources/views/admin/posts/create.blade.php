70f25e082a391baa7d9b47a803944d869984ca3f
salamone-gius <salamone.gius@gmail.com>
2022-07-19 19:21:32 +0200

    form set

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,7 +7,23 @@
             <h1>Crea un nuovo post</h1>
         </div>
         <div class="card-body">
-            {{-- qui metterò un form con tutti i campi della tabella 'posts' che punta alla rotta dello store (che andrà poi ad immagazzinare i dati inseriti) --}}
+            <form action="{{route('admin.posts.index')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Create</button>
+            </form>
         </div>
     </div>
 </div>
