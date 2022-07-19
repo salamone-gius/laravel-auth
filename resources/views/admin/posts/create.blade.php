3cd36a2097c06ed140d558d178af1942b3fc3c32
salamone-gius <salamone.gius@gmail.com>
2022-07-19 18:48:34 +0200

    layout extended and card created in 'create.blade'

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,1 +1,15 @@
-<h1>Crea Post</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1>Crea un nuovo post</h1>
+        </div>
+        <div class="card-body">
+            {{-- qui metterò un form con tutti i campi della tabella 'posts' che punta alla rotta dello store (che andrà poi ad immagazzinare i dati inseriti) --}}
+        </div>
+    </div>
+</div>
+    
+@endsection
