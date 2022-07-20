735f2596ac714024dff0917023c09590617d6aab
salamone-gius <salamone.gius@gmail.com>
2022-07-20 15:43:47 +0200

    validation error messages added

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,14 +4,17 @@
 <div class="container">
     <div class="card">
         <div class="card-header">
-            <h1>Crea un nuovo post</h1>
+            <h1>Crete new post</h1>
         </div>
         <div class="card-body">
             <form action="{{route('admin.posts.index')}}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title">
+                    @error('title')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
                 {{-- si potrebbe aggiungere la WYSIWYG ckeditor (textarea con possibilità di personalizzare il testo), ma poi i dati che ritornerebbero sarebbero troppo complessi da gestire (per il momento)  --}}
                 {{-- <div class="form-group">
@@ -20,11 +23,17 @@
                 </div> --}}
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <textarea class="form-control" id="content" name="content" rows="6"></textarea>
+                    <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="6"></textarea>
+                    @error('content')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
                 <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="published" name="published">
+                    <input type="checkbox" class="form-check-input @error('published') is-invalid @enderror" id="published" name="published">
                     <label class="form-check-label" for="published">Post</label>
+                    @error('published')
+                        <div class="alert alert-danger">{{$message}}</div>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Create</button>
             </form>
